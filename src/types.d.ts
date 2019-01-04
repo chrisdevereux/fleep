b476a37558525caeb6eea3f993e4358410ba44be
Chris Devereux <devereux.chris@gmail.com>
2019-01-04 15:24:42 +0000

    initial

---
+++ src/types.d.ts
@@ -0,0 +1,6 @@
+declare module "react-keyframes" {
+  import React from 'react'
+
+  export const Keyframes: React.ComponentType<{loop?: number | true}>
+  export const Frame: React.ComponentType<{duration: number}>
+}
